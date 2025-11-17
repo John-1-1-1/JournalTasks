bac22cb1181f3170c318ca1f34e32971c1977ea1
John-1-1-1 <john_1_1_1@mail.ru>
2025-11-17 15:48:01 +0700

    DB Commits

---
+++ JournalTasks.Web/Data/Migrations/20251116132555_Add_IsTracking_TaskModel.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JournalTasks.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_IsTracking_TaskModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskModels_AspNetUsers_UserId",
+                table: "TaskModels");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "TaskModels",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTracking",
+                table: "TaskModels",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskModels_AspNetUsers_UserId",
+                table: "TaskModels",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskModels_AspNetUsers_UserId",
+                table: "TaskModels");
+
+            migrationBuilder.DropColumn(
+                name: "IsTracking",
+                table: "TaskModels");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "TaskModels",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskModels_AspNetUsers_UserId",
+                table: "TaskModels",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ JournalTasks.Web/Data/Migrations/20251116134119_Add_TimeTracker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JournalTasks.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_TimeTracker : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DeltaTimes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TaskId = table.Column<int>(type: "INTEGER", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: false),
+                    TaskModelId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeltaTimes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DeltaTimes_TaskModels_TaskModelId",
+                        column: x => x.TaskModelId,
+                        principalTable: "TaskModels",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeltaTimes_TaskModelId",
+                table: "DeltaTimes",
+                column: "TaskModelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeltaTimes");
+        }
+    }
+}
